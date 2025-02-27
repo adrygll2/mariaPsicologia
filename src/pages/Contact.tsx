829166bf26f665223be3bd2b4a840d5cffb8cad1
Adrygll2 <nb17adri@gmail.com>
2025-02-27 12:09:17 +0100

    Added serverless functions

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -28,7 +28,7 @@ export function Contact() {
         body: JSON.stringify(formData),
       });*/
 
-      let response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/.netlify/functions/sendEmail`, {
+      let response = await fetch(`/.netlify/functions/sendEmail`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
